a49118e308e017399305fa3e7632f3db2f3e62aa
Alexa-Tuchtenhagen <92337979+Alexa-Tuchtenhagen@users.noreply.github.com>
2025-11-17 13:34:32 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -366,7 +366,7 @@ function App() {
                   /*Edit Here*/
                   projects: [
                     { name: 'Robotic Arms', description: 'Robotic Arms is a dual-arm robotic system featuring a lead-and-follow design, allowing one arm to mirror the motion of the other. It can be manually taught and recorded to perform tasks, demonstrating a real-world robotic solution focused on intuitive programming and motion control.', codeUrl: 'https://huggingface.co/docs/lerobot/en/koch', linkUrl: '/Robotic Arms.pdf', image: '/Robotic Arms.jpeg' },
-                    { name: 'VEXU Ring Bot', description: 'This VEX U robot was designed to pick up game rings and accurately place them on goal posts. Our team engineered a reliable ring-intake and scoring system, demonstrating a hands-on robotic solution built to address a real competition challenge', codeUrl: 'https://github.com/Stormpiethon/VEX_Stuff/tree/main/vex-vscode-projects/big_boi/src', linkUrl: '/VEXU Ring Bot.pdf', image: '/Ring Bot.jpeg' }
+                    { name: 'VEXU Ring Bot', description: 'This VEX U robot was designed to pick up game rings and accurately place them on goal posts. Our team engineered a reliable ring-intake and scoring system, demonstrating a hands-on robotic solution built to address a real competition challenge', codeUrl: 'https://github.com/Stormpiethon/VEX_Stuff/tree/main/vex-vscode-projects/big_boi/src', linkUrl: '/VEXU Ring Bot V2.pdf', image: '/Ring Bot.jpeg' }
                   ]
                 },
                 {
